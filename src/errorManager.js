bb7a110ccb64bfc8a7138f9762e4ded342fb20c3
Jason Quense <monastic.panic@gmail.com>
2016-08-09 14:23:18 -0400

    [fixed] error update bug

--- src/errorManager.js
+++ src/errorManager.js
@@ -23,30 +23,29 @@ export default function errorManager(handleValidation) {
   }
 
   return {
-    collect(paths, errors = {}, options) {
+    collect(paths, pristineErrors = {}, options) {
       paths = pathUtils.reduce([].concat(paths))
 
-      let validations = Array(paths.length)
+      let errors = { ...pristineErrors }
+      let nextErrors = errors
       let workDone = false
-      let newErrors = errors
 
-      paths.forEach((path, index) => {
-        newErrors = pathUtils.trim(path, newErrors)
-        if (!workDone && errors !== newErrors) {
+      let validations = paths.map(path => {
+        nextErrors = pathUtils.trim(path, nextErrors)
+
+        if (errors !== nextErrors) {
           workDone = true;
-
-          newErrors = { ...newErrors }
         }
 
-        validations[index] = validatePath(path, options, newErrors)
+        return validatePath(path, options, nextErrors)
       })
 
       return Promise.all(validations)
         .then(results => {
           if (!workDone && results.every(Boolean))
-            return errors
+            return pristineErrors
 
-          return newErrors
+          return nextErrors
         })
     }
   }
