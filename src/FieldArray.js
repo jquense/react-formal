deea284237ba46d9490b0407519b2479c71a0161
Steven Loria <sloria1@gmail.com>
2019-04-23 23:08:01 -0400

    docs: don't shadow variable and exemplify arrayHelpers.add (#162)
    `.add` is more intuitive in the UI example and probably
    more common.

--- src/FieldArray.js
+++ src/FieldArray.js
@@ -44,12 +44,12 @@ function filter(errors, baseName) {
  *   <Form.FieldArray name="friends" events="blur">
  *    {({ value, arrayHelpers }) => (
  *       <ul>
- *        {value.map((value, idx) => (
+ *        {value.map((item, idx) => (
  *          <li key={idx} >
  *            <div style={{ display: 'flex', alignItems: 'flex-start' }}>
  *              <Form.Field name={`friends[${idx}].name`} />
- *              <button type="button" onClick={() => arrayHelpers.remove(value)}>-</button>
- *              <button type="button" onClick={() => arrayHelpers.insert({ name: undefined }, idx)}>+</button>
+ *              <button type="button" onClick={() => arrayHelpers.remove(item)}>-</button>
+ *              <button type="button" onClick={() => arrayHelpers.add({ name: undefined }, idx)}>+</button>
  *            </div>
  *            <Form.Message for={`friends[${idx}].name`} />
  *          </li>
