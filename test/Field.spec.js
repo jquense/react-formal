864fa31fa8fea7c20673cc19219fb516775fe875
Jason Quense <monastic.panic@gmail.com>
2020-02-27 14:45:19 -0500

    WIP

--- test/Field.spec.js
+++ test/Field.spec.js
@@ -1,8 +1,8 @@
-import { mount } from 'enzyme'
-import React from 'react'
-import { act } from 'react-dom/test-utils'
-import * as yup from 'yup'
-import Form from '../src'
+import { mount } from 'enzyme';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import * as yup from 'yup';
+import Form from '../src';
 
 describe('Field', () => {
   let schema = yup.object({
@@ -14,7 +14,7 @@ describe('Field', () => {
         isCool: yup.bool(),
       }),
     }),
-  })
+  });
 
   class TestInput extends React.Component {
     render() {
@@ -23,7 +23,7 @@ describe('Field', () => {
           value={this.props.value || ''}
           onChange={e => this.props.onChange(e, 'hi')}
         />
-      )
+      );
     }
   }
 
@@ -35,8 +35,8 @@ describe('Field', () => {
     )
       .find(TestInput)
       .instance()
-      .props.className.should.include('test') // test invalid-field
-  })
+      .props.className.should.include('test'); // test invalid-field
+  });
 
   it('should fall back to using schema types', () => {
     let schema = yup.object({
@@ -44,7 +44,7 @@ describe('Field', () => {
       number: yup.number(),
       date: yup.date(),
       bool: yup.bool(),
-    })
+    });
 
     let wrapper = mount(
       <Form schema={schema} defaultValue={{}}>
@@ -55,17 +55,17 @@ describe('Field', () => {
         <Form.Field as="select" name="string" />
         <Form.Field as="textarea" name="string" />
       </Form>,
-    )
+    );
 
     // console.log(wrapper.debug())
-    wrapper.assertSingle(`input[name='string']`)
-    wrapper.assertSingle('input[type="number"]')
-    wrapper.assertSingle('input[type="date"]')
+    wrapper.assertSingle(`input[name='string']`);
+    wrapper.assertSingle('input[type="number"]');
+    wrapper.assertSingle('input[type="date"]');
 
-    wrapper.assertSingle('input[type="checkbox"]')
-    wrapper.assertSingle('select')
-    wrapper.assertSingle('textarea')
-  })
+    wrapper.assertSingle('input[type="checkbox"]');
+    wrapper.assertSingle('select');
+    wrapper.assertSingle('textarea');
+  });
 
   it('should use as override', () => {
     let wrapper = mount(
@@ -74,11 +74,11 @@ describe('Field', () => {
         <Form.Field name="name" as="textarea" />
         <Form.Field name="name" as={TestInput} />
       </Form>,
-    )
-    wrapper.assertSingle(TestInput)
-    wrapper.assertSingle('textarea')
-    wrapper.assertSingle('select')
-  })
+    );
+    wrapper.assertSingle(TestInput);
+    wrapper.assertSingle('textarea');
+    wrapper.assertSingle('select');
+  });
 
   it('should fire onChange', done => {
     mount(
@@ -87,17 +87,17 @@ describe('Field', () => {
           name="name"
           as={TestInput}
           onChange={() => {
-            done()
+            done();
           }}
         />
       </Form>,
     )
       .assertSingle('input')
-      .simulate('change')
-  })
+      .simulate('change');
+  });
 
   it('should pull value from event target', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
 
     mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
@@ -106,36 +106,36 @@ describe('Field', () => {
     )
       //.tap(_ => console.log(_.debug()))
       .assertSingle('input')
-      .simulate('change', { target: { value: 'foo' } })
+      .simulate('change', { target: { value: 'foo' } });
 
-    spy.should.have.been.calledWith({ name: 'foo' })
-  })
+    spy.should.have.been.calledWith({ name: 'foo' });
+  });
 
   it('should coerce value to number', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
 
     let form = mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
         <Form.Field name="age" id="first" />
         <Form.Field type="range" name="age" id="second" />
       </Form>,
-    )
+    );
 
     form
       .find('input[type="number"]')
-      .simulate('change', { target: { value: '3.56', type: 'number' } })
+      .simulate('change', { target: { value: '3.56', type: 'number' } });
 
-    spy.should.have.been.calledWith({ age: 3.56 })
+    spy.should.have.been.calledWith({ age: 3.56 });
 
     form
       .find('input[type="range"]')
-      .simulate('change', { target: { value: '42', type: 'range' } })
+      .simulate('change', { target: { value: '42', type: 'range' } });
 
-    spy.should.have.been.calledWith({ age: 42 })
-  })
+    spy.should.have.been.calledWith({ age: 42 });
+  });
 
   it('should update touched value', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
 
     mount(
       <Form schema={schema} defaultValue={{}} onTouch={spy}>
@@ -143,13 +143,13 @@ describe('Field', () => {
       </Form>,
     )
       .assertSingle('input')
-      .simulate('change', 'foo')
+      .simulate('change', 'foo');
 
-    spy.should.have.been.calledWith({ name: true }, ['name'])
-  })
+    spy.should.have.been.calledWith({ name: true }, ['name']);
+  });
 
   it('should update touched once per field', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
 
     mount(
       <Form schema={schema} defaultValue={{}} onTouch={spy}>
@@ -158,49 +158,49 @@ describe('Field', () => {
     )
       .assertSingle('input')
       .simulate('change', 'foo')
-      .simulate('change', 'bar')
+      .simulate('change', 'bar');
 
-    spy.callCount.should.equal(1)
-  })
+    spy.callCount.should.equal(1);
+  });
 
   it('ensures values are never undefined', () => {
     let wrapper = mount(
       <Form schema={schema} defaultValue={{}}>
         <Form.Field name="name" />
       </Form>,
-    )
+    );
 
-    expect(wrapper.assertSingle('input').prop('value')).to.equal('')
-  })
+    expect(wrapper.assertSingle('input').prop('value')).to.equal('');
+  });
 
   it('maps value from string', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
     mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
         <Form.Field name="name" as={TestInput} mapFromValue="value" />
       </Form>,
     )
       .assertSingle('input')
-      .simulate('change', { value: 'john' })
+      .simulate('change', { value: 'john' });
 
-    spy.should.have.been.calledOnce.and.calledWith({ name: 'john' })
-  })
+    spy.should.have.been.calledOnce.and.calledWith({ name: 'john' });
+  });
 
   it('maps value from function', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
     mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
         <Form.Field name="name" as={TestInput} mapFromValue={e => e.value} />
       </Form>,
     )
       .assertSingle('input')
-      .simulate('change', { value: 'john' })
+      .simulate('change', { value: 'john' });
 
-    spy.should.have.been.calledOnce.and.calledWith({ name: 'john' })
-  })
+    spy.should.have.been.calledOnce.and.calledWith({ name: 'john' });
+  });
 
   it('gets value from accessor', () => {
-    let spy = sinon.spy(model => model.other)
+    let spy = sinon.spy(model => model.other);
     let wrapper = mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
         <Form.Field
@@ -212,17 +212,17 @@ describe('Field', () => {
           }}
         />
       </Form>,
-    )
+    );
 
-    spy.should.have.been.and.calledWith({})
+    spy.should.have.been.and.calledWith({});
 
-    wrapper.assertSingle('input').simulate('change', { value: 'john' })
+    wrapper.assertSingle('input').simulate('change', { value: 'john' });
 
-    spy.should.have.been.and.calledWith({ other: 'john' })
-  })
+    spy.should.have.been.and.calledWith({ other: 'john' });
+  });
 
   it('maps values from hash', () => {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
     mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
         <Form.Field
@@ -236,78 +236,81 @@ describe('Field', () => {
       </Form>,
     )
       .assertSingle('input')
-      .simulate('change', { value: 'john', text: 'hi' })
+      .simulate('change', { value: 'john', text: 'hi' });
 
-    spy.should.have.been.calledOnce.and.calledWith({ name: 'john', text: 'hi' })
-  })
+    spy.should.have.been.calledOnce.and.calledWith({
+      name: 'john',
+      text: 'hi',
+    });
+  });
 
   it('should pass all args to mapFromValue', function(done) {
-    let spy = sinon.spy()
+    let spy = sinon.spy();
     mount(
       <Form schema={schema} defaultValue={{}} onChange={spy}>
         <Form.Field
           name="name"
           as={TestInput}
           mapFromValue={(...args) => {
-            args.length.should.equal(2)
-            args[1].should.equal('hi')
-            done()
+            args.length.should.equal(2);
+            args[1].should.equal('hi');
+            done();
           }}
         />
       </Form>,
     )
       .assertSingle('input')
-      .simulate('change')
-  })
+      .simulate('change');
+  });
 
   it('should forward inner ref', () => {
-    let inst
+    let inst;
     mount(
       <Form schema={schema} defaultValue={{}}>
         <Form.Field
           name="name"
           as={TestInput}
           ref={r => {
-            inst = r
+            inst = r;
           }}
         />
       </Form>,
-    )
-    ;(inst instanceof TestInput).should.be.true()
-  })
+    );
+    (inst instanceof TestInput).should.be.true();
+  });
 
   it('should work with conditional schema', () => {
-    const spy = sinon.stub(console, 'warn').callsFake(() => {})
+    const spy = sinon.stub(console, 'warn').callsFake(() => {});
 
     let render = name => {
       mount(
         <Form schema={schema} defaultValue={{ ...schema.default(), name }}>
           <Form.Field name="more.isCool" />
         </Form>,
-      )
-    }
+      );
+    };
 
-    render('john')
-    expect(spy).to.have.been.called()
-  })
+    render('john');
+    expect(spy).to.have.been.called();
+  });
 
-  describe.only('meta', () => {
+  describe('meta', () => {
     it('should pass meta to field', done => {
       let Input = ({ meta }) => {
         // //first pass isn't correct since form hasn't propagated it's state yet.
         // if (!meta.invalid) return null
 
-        meta.invalid.should.equals(true)
-        meta.valid.should.equals(false)
+        meta.invalid.should.equals(true);
+        meta.valid.should.equals(false);
 
-        meta.touched.should.equals(true)
+        meta.touched.should.equals(true);
 
         meta.errors.should.eqls({
           name: 'foo',
-        })
-        done()
-        return null
-      }
+        });
+        done();
+        return null;
+      };
 
       mount(
         <Form
@@ -318,29 +321,29 @@ describe('Field', () => {
         >
           <Form.Field name="name" as={Input} />
         </Form>,
-      )
-    })
+      );
+    });
 
     it('should pass meta to field with noValidate', done => {
       let Input = ({ meta }) => {
-        meta.invalid.should.equals(true)
-        meta.valid.should.equals(false)
+        meta.invalid.should.equals(true);
+        meta.valid.should.equals(false);
         meta.errors.should.eqls({
           name: 'foo',
-        })
-        done()
-        return null
-      }
+        });
+        done();
+        return null;
+      };
 
       mount(
         <Form schema={schema} defaultValue={{}} defaultErrors={{ name: 'foo' }}>
           <Form.Field noValidate name="name" as={Input} />
         </Form>,
-      )
-    })
+      );
+    });
 
     it('should field onError should remove existing errors', () => {
-      let errorSpy = sinon.spy()
+      let errorSpy = sinon.spy();
       act(() => {
         mount(
           <Form
@@ -354,14 +357,14 @@ describe('Field', () => {
         )
           .find(TestInput)
           .props()
-          .meta.onError({})
-      })
+          .meta.onError({});
+      });
 
-      errorSpy.should.have.been.calledOnce.and.calledWith({ bar: 'baz' })
-    })
+      errorSpy.should.have.been.calledOnce.and.calledWith({ bar: 'baz' });
+    });
 
     it('should field onError should update field errors', () => {
-      let errorSpy = sinon.spy()
+      let errorSpy = sinon.spy();
       act(() => {
         mount(
           <Form
@@ -375,21 +378,21 @@ describe('Field', () => {
         )
           .find(TestInput)
           .props()
-          .meta.onError({ name: 'foo', 'name.first': 'baz' })
-      })
+          .meta.onError({ 'name': 'foo', 'name.first': 'baz' });
+      });
 
       errorSpy.should.have.been.calledOnce.and.calledWith({
-        name: 'foo',
+        'name': 'foo',
         'name.first': 'baz',
-        bar: 'baz',
-      })
-    })
+        'bar': 'baz',
+      });
+    });
 
-    it.only('should set events via a function', done => {
+    it('should set events via a function', done => {
       let schema = yup.object({
         number: yup.number().min(5),
-      })
-      let spy = sinon.spy()
+      });
+      let spy = sinon.spy();
       let wrapper = mount(
         <Form
           delay={0}
@@ -402,30 +405,30 @@ describe('Field', () => {
             events={({ valid }) => (valid ? ['onBlur'] : ['onChange'])}
           />
         </Form>,
-      )
+      );
       // Field is valid only; `onBlur`
       act(() => {
-        wrapper.find('input').simulate('change', { target: { value: '4' } })
-        wrapper.find('input').simulate('blur', { target: { value: '4' } })
-      })
+        wrapper.find('input').simulate('change', { target: { value: '4' } });
+        wrapper.find('input').simulate('blur', { target: { value: '4' } });
+      });
       setTimeout(() => {
         act(() => {
-          spy.callCount.should.equal(1)
+          spy.callCount.should.equal(1);
           // field is invalid now: `onChange`
-          wrapper.find('input').simulate('blur', { target: { value: '4' } })
+          wrapper.find('input').simulate('blur', { target: { value: '4' } });
 
-          spy.callCount.should.equal(1)
+          spy.callCount.should.equal(1);
 
-          wrapper.find('input').simulate('change', { target: { value: '6' } })
+          wrapper.find('input').simulate('change', { target: { value: '6' } });
 
-          spy.callCount.should.equal(2)
-        })
-        done()
-      }, 100)
-    })
+          spy.callCount.should.equal(2);
+        });
+        done();
+      }, 100);
+    });
 
     it('should field onError should replace field errors', () => {
-      let errorSpy = sinon.spy()
+      let errorSpy = sinon.spy();
 
       act(() => {
         mount(
@@ -440,15 +443,15 @@ describe('Field', () => {
         )
           .find(TestInput)
           .props()
-          .meta.onError({ 'name.first': 'baz' })
-      })
+          .meta.onError({ 'name.first': 'baz' });
+      });
 
       errorSpy.should.have.been.calledOnce.and.calledWith({
         'name.first': 'baz',
-        bar: 'baz',
-      })
-    })
-  })
+        'bar': 'baz',
+      });
+    });
+  });
 
   describe('inclusive active matching', () => {
     it('should count path matches', () => {
@@ -456,8 +459,8 @@ describe('Field', () => {
         <Form schema={schema} defaultValue={{}} defaultErrors={{ name: 'foo' }}>
           <Form.Field name="name" errorClass="invalid" />
         </Form>,
-      ).assertSingle('input.invalid')
-    })
+      ).assertSingle('input.invalid');
+    });
 
     it('should use inclusive active checking', () => {
       mount(
@@ -468,8 +471,8 @@ describe('Field', () => {
         >
           <Form.Field name="name" errorClass="invalid" />
         </Form>,
-      ).assertSingle('input.invalid')
-    })
+      ).assertSingle('input.invalid');
+    });
 
     it('should respect `exclusive`', () => {
       mount(
@@ -480,17 +483,17 @@ describe('Field', () => {
         >
           <Form.Field exclusive name="name" errorClass="invalid" />
         </Form>,
-      ).assertNone('input.invalid')
-    })
+      ).assertNone('input.invalid');
+    });
 
     it('should respect `exclusive` and still have errors', () => {
       mount(
         <Form schema={schema} defaultValue={{}} defaultErrors={{ name: 'foo' }}>
           <Form.Field exclusive name="name" errorClass="invalid" />
         </Form>,
-      ).assertSingle('input.invalid')
-    })
-  })
+      ).assertSingle('input.invalid');
+    });
+  });
 
   xdescribe('form fields', () => {
     it('should inject onError', () => {
@@ -501,8 +504,8 @@ describe('Field', () => {
       )
         .find('input')
         .prop('onError')
-        .should.be.a('function')
-    })
+        .should.be.a('function');
+    });
 
     // skip for now since name is still required.
     xit('should not inject onError for nameless fields', () => {
@@ -513,11 +516,11 @@ describe('Field', () => {
       )
         .find('input')
         .prop('onError')
-        .should.be.a('function')
-    })
+        .should.be.a('function');
+    });
 
     it('should propagate onError to form', () => {
-      let spy = sinon.spy()
+      let spy = sinon.spy();
 
       mount(
         <Form schema={schema} defaultValue={{}} onError={spy}>
@@ -525,12 +528,12 @@ describe('Field', () => {
         </Form>,
       )
         .find('input')
-        .prop('onError')({ foo: 'bar' })
+        .prop('onError')({ foo: 'bar' });
 
       spy.should.have.been.calledOnce.and.calledWith({
         'name.foo': 'bar',
-      })
-    })
+      });
+    });
 
     it('should properly prefix nested errors', () => {
       const onError = mount(
@@ -539,11 +542,13 @@ describe('Field', () => {
         </Form>,
       )
         .find('input')
-        .prop('onError')
+        .prop('onError');
 
-      onError({ foo: 'bar' }).should.eql({ 'name.foo': 'bar' })
-      onError({ '[1].foo': 'bar' }).should.eql({ 'name[1].foo': 'bar' })
-      onError({ '[1].baz.foo': 'bar' }).should.eql({ 'name[1].baz.foo': 'bar' })
-    })
-  })
-})
+      onError({ foo: 'bar' }).should.eql({ 'name.foo': 'bar' });
+      onError({ '[1].foo': 'bar' }).should.eql({ 'name[1].foo': 'bar' });
+      onError({ '[1].baz.foo': 'bar' }).should.eql({
+        'name[1].baz.foo': 'bar',
+      });
+    });
+  });
+});
